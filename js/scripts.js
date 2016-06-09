e3ba1df353e6673612ed2adb8ab336aba6cf0935
CassandraO <cassandra.oppedal@eagles.ewu.edu>
2016-06-09 01:34:22 -0700

    Animations complete

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,35 @@ var $vcdDoodle = $('#vcdDoodle');
 // DOM Element, Duration, CSS Properties
 TweenMax.to($vcdDoodle, 2, {
     opacity: 100,
-    
+   
 	y: 200
 });
+
+
 
 //Animate thumbtack
 var $thumbtack = $('#thumbtack');
 
+
+
 // DOM Element, Duration, CSS Properties
-TweenMax.from($thumbtack, 3, {
-    opacity: 0,
+TweenMax.to($thumbtack, 1, {
+    opacity: 100,
 	delay: 1,
 	ease: Linear.easeIn,
-    y: 300,
-	x: 445
+    y: -100,
+	x: -239
+	
+});
+
+var $grade = $('#grade');
+
+
+TweenMax.to($grade, 2.5, {
+    opacity: 100,
+	delay: 1,
+	ease: Linear.easeIn,
+    y: -188,
+	x: -145
 	
 });
