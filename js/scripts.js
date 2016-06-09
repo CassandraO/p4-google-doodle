fa32be9578ce6ab3d4d9365d55bc802ea12cf926
CassandraO <cassandra.oppedal@eagles.ewu.edu>
2016-06-08 22:03:34 -0700

    Animations In Progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 
 
-//Animate Star
-var $star = $('#star');
+
+
+
+//Animate vcdDoodle
+var $vcdDoodle = $('#vcdDoodle');
 
 // DOM Element, Duration, CSS Properties
-TweenMax.to($star, 1, {
+TweenMax.to($vcdDoodle, 2, {
+    opacity: 100,
+    
+	y: 200
+});
+
+//Animate thumbtack
+var $thumbtack = $('#thumbtack');
+
+// DOM Element, Duration, CSS Properties
+TweenMax.from($thumbtack, 3, {
     opacity: 0,
-    x: 180
+	delay: 1,
+	ease: Linear.easeIn,
+    y: 300,
+	x: 445
+	
 });
